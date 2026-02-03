d93461579322aac3f57e8babc59871382c6e0cd5
ShriramShanbhag <shriramshanbhag@hotmail.com>
2026-02-03 17:21:13 +0530

    set up db connecttion before starting server

--- app.js
+++ app.js
@@ -3,4 +3,8 @@ import express from 'express';
 const app = express();
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.send('Welcome to the URL Shortener Service!');
+});
+
 export default app;
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import dotenv from 'dotenv';
 import app from './app.js';
+import { testDBConnection } from './src/config/db.js';
 
 dotenv.config();
 
 const startServer = async () => {
     try {
+        await testDBConnection()
         app.listen(process.env.PORT, () => {
         console.log(`Server is running on port ${process.env.PORT}`);
     });
