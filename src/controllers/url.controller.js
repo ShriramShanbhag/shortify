f4c1fa3d7b2d3266d496405685041e204c0e7dbe
ShriramShanbhag <shriramshanbhag@hotmail.com>
2026-02-07 08:51:00 +0530

    updated controller

--- src/controllers/url.controller.js
+++ src/controllers/url.controller.js
@@ -1,9 +1,9 @@
 import UrlService from "../services/url.service.js";
 export const shorten = async (req, res, next) => {
     try {
-        const { originalURL } = req.body;
-        if(!originalURL) return res.status(400).json({ error: 'Original URL is required' });
-        const result = await UrlService.shorten(originalURL);
+        const { url } = req.body;
+        if(!url) return res.status(400).json({ error: 'Original URL is required' });
+        const result = await UrlService.shorten(url);
         res.json(result);
     } catch (error) {
         next(error)
@@ -11,9 +11,9 @@ export const shorten = async (req, res, next) => {
 }
 export const redirect = async (req, res, next) => {
     try {
-        const { shortCode } = req.params;
-        if(!shortCode) return res.status(400).json({ error: 'Short code is required' });
-        const originalURL = await UrlService.getOriginal(shortCode);
+        const { code } = req.params;
+        if(!code) return res.status(400).json({ error: 'Short code is required' });
+        const originalURL = await UrlService.getOriginal(code);
         if(!originalURL) return res.status(404).json({ error: 'URL not found' });
         res.redirect(302, originalURL);
     } catch (error) {
